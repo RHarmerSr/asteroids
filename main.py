9a184fecf3d717562ac4c4d771d113363c2609d2
RHarmerSr <rharmersr@gmail.com>
2025-08-08 13:05:17 -0400

    Added player and player movement

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = updatable, drawable
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
+    clock = pygame.time.Clock() # Create a clock to control the frame rate
+    dt = 0  # Initialize delta time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Create a player instance at the center of the screen
+
 
     while True:
         screen.fill("black")  # Fill the screen with black
+        player.update(dt)  # Update the player
+        player.draw(screen)  # Draw the player
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000.0  # Calculate delta time in seconds
+        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
